78767a47dc1bd30f2dfbd248302c3840a7dadefe
Derby1537 <dellerba.gabriele@gmail.com>
2026-03-19 14:38:42 +0100

    finished script

--- save_encoding.py
+++ save_encoding.py
@@ -1,5 +1,9 @@
 import face_recognition
 import sys
+import pickle
+from db.db import engine
+from sqlalchemy.orm import sessionmaker
+from models.Person import Person
 
 if len(sys.argv) < 2:
     print("Try: python save_encoding.py <filename> <person's name>")
@@ -7,11 +11,26 @@ if len(sys.argv) < 2:
 
 image_path = sys.argv[1]
 
-name = sys.argv[2]
-for arg in sys.argv[3:]:
-    name = name + " " + arg
+name = " ".join(sys.argv[2:])
 
 image = face_recognition.load_image_file(image_path)
-encoding = face_recognition.face_encodings(image)[0]
+encodings = face_recognition.face_encodings(image)
 
-print(encoding)
+if not(encodings):
+    print(f"No person found in picture {image_path}")
+    exit()
+
+encoding = encodings[0]
+
+encoding_blob = pickle.dumps(encoding)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+person = Person(name=name, encoding=encoding_blob)
+
+session.add(person)
+session.commit()
+session.close()
+
+print(f"Person {name} saved successfully")
